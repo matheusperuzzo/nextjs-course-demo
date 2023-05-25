84932abec17eb1e83ed1adfa183f5faecad1c624
matheusperuzzo <matheusperuzzodev@gmail.com>
2023-05-25 16:25:16 -0400

    lecture 359

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,7 +1,22 @@
-export default function handler(req, res) {
+import { MongoClient } from "mongodb";
+
+export default async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    const client = await MongoClient.connect(
+      "mongodb+srv://admin:admin@cluster0.m12571f.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
   }
 }
