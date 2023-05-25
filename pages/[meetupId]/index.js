2c51bfb09598af672a42ce7b925ac945f15bad8e
matheusperuzzo <matheusperuzzodev@gmail.com>
2023-05-25 15:55:03 -0400

    lecture 356

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -10,3 +10,24 @@ export default function MeetupDetails() {
     />
   );
 }
+
+export async function getStaticProps(context) {
+  //fetch data for single meetup
+
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://englishlive.ef.com/pt-br/blog/wp-content/uploads/sites/16/2016/02/City-ou-Town.jpg",
+        id: meetupId,
+        title: "First Meetup",
+        address: "Some Street 5, Some City",
+        description: "This is a first meetup",
+      },
+    },
+  };
+}
