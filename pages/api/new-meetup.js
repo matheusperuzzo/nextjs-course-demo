9cdd75c91d68536a2bd6d5698f581c7880f8d87f
matheusperuzzo <matheusperuzzodev@gmail.com>
2023-05-25 17:12:53 -0400

    lecture 360

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,13 +1,14 @@
 import { MongoClient } from "mongodb";
+const uri =
+  "mongodb+srv://admin:k42tgJL5bR1pOp0w@cluster0.m12571f.mongodb.net/meetups?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri);
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
 
-    const client = await MongoClient.connect(
-      "mongodb+srv://admin:admin@cluster0.m12571f.mongodb.net/meetups?retryWrites=true&w=majority"
-    );
-    const db = client.db();
+    const db = client.db("meetups");
 
     const meetupsCollection = db.collection("meetups");
 
